23e6511484f6fac485758ff0230d22b6e7750f3e
veerasagar <veerasagar24502@gmail.com>
2025-02-16 20:24:49 +0530

    rm basic_code = others code

--- Basic code/5/src/Data/Circuits/AbuDhabi.java
+++
@@ -1,7 +0,0 @@
-package Data.Circuits;
-
-public class AbuDhabi {
-	public static final int NUM_LAPS = 55;
-	public static final double OVERTAKING_THRESHOLD = -1.5;
-	public static final double OVERTAKING_PROBABILITY = 0.3;
-}
